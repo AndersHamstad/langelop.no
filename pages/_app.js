cce20f4fa45e0cf5e5e581c9d77e3febf82d7517
AndersHamstad <andershamstad@gmail.com>
2025-04-28 14:06:33 +0200

    Legger til SEO, Google Analytics og små forbedringer

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,40 @@
 import '../styles/globals.css';
 import '../styles/Datepicker.css';
 import 'react-datepicker/dist/react-datepicker.css';
-import Script from 'next/script';
+import Head from 'next/head';
+import Script from 'next/script'; // 👈 Importerer Next.js sin Script-komponent
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* SEO og Meta-informasjon */}
+      <Head>
+        <title>Ultraløp i Norge | langeløp.no</title>
+        <meta name="description" content="Oversikt over alle ultraløp i Norge. Finn din neste utfordring, få tips, erfaringer og inspirasjon fra andre løpere!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Ultraløp i Norge | langeløp.no" />
+        <meta property="og:description" content="Finn ultraløp i Norge, få tips og del erfaringer fra landets vakreste løp." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.langelop.no" />
+        <meta property="og:image" content="https://www.langelop.no/hero-2.jpg" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       {/* Google Analytics */}
-      <Script 
-        src="https://www.googletagmanager.com/gtag/js?id=G-ZW1DPYXS0N" 
-        strategy="afterInteractive" 
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZW1DPYXS0N"
+        strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-ZW1DPYXS0N', {
-            page_path: window.location.pathname,
-          });
+          gtag('config', 'G-ZW1DPYXS0N');
         `}
       </Script>
 
-      {/* Resten av appen */}
+      {/* Hovedinnhold */}
       <Component {...pageProps} />
     </>
   );
