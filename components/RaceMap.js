1859210244f8fb554e4353f594273bd8785a2581
Anders Hamstad <andershamstad@Anders-sin-MacBook-Air.local>
2026-04-25 20:01:07 +0200

    Bytt til kompatibel kart-clustering

--- components/RaceMap.js
+++ components/RaceMap.js
@@ -1,5 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-cluster";
+import MarkerClusterGroup from "@changey/react-leaflet-markercluster";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import Link from "next/link";
 import L from "leaflet";
 import "leaflet.markercluster/dist/MarkerCluster.css";
